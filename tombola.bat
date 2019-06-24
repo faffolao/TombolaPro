be9f9f96f5110e9632cc1a21f1692e87b917a087
Federico Arduini <federico_arduini@live.it>
2019-06-24 14:39:55 +0200

    tombola.bat - internal strings correction

--- tombola.bat
+++ tombola.bat
@@ -2,7 +2,7 @@
 cls
 echo --{ Avvio tombola in corso }--
 echo "Attenzione: Assicurarsi di avere l'ultima versione di Node.js installata per poter avviare la tombola correttamente!"
-echo Usa questo file se stai usando macOS oppure Linux.
+echo Usa questo file se stai usando Windows.
 echo.
 echo.
 cd Tombola_desktop
